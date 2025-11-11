0b35a45d4d1675dd94cd4ada13ce8889b683fee7
boaz-ng <bkn2545@gmail.com>
2025-11-11 13:29:11 -0500

    Updated to cache chunks of documents

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,17 +1,42 @@
 from FlagEmbedding import FlagModel
 import json
+import os
 
 def main():
     model = FlagModel('BAAI/bge-base-en-v1.5', 
                   query_instruction_for_retrieval="Represent this sentence for searching relevant passages: ",
-                  use_fp16=True)
+                  use_fp16=False)
     
-    with open('documents.json', 'r') as in_f, open('preprocessed_documents.json', 'w') as out_f:
-        data = json.load(in_f)
-        sentences: list[str] = [item['text'] for item in data]
-        embeddings = model.encode(sentences, batch_size=128) # experiment to see what runs faster
-        data = [{'id': item['id'], 'text': item['text'], 'embedding': embedding.tolist()} for item, embedding in zip(data, embeddings)]
-        json.dump(data, out_f)
+    with open('documents.json', 'r') as in_f:
+        docs = json.load(in_f)
+
+    preprocessed = []
+    if os.path.exists('preprocessed_documents.json') and os.path.getsize('preprocessed_documents.json') > 0:
+        with open('preprocessed_documents.json', 'r') as out_f:
+            preprocessed = json.load(out_f)
+
+    start = len(preprocessed)
+    for i in range(start, len(docs), 128):
+        batch_docs = docs[i:i+128]
+        batch = [doc['text'] for doc in batch_docs]
+        embeddings = model.encode(batch, batch_size=1) # experiment to see what runs faster
+
+        batch_data = [
+            {
+                'id': item['id'], 
+                'text': item['text'], 
+                'embedding': embedding.tolist()
+            } 
+            for item, embedding in zip(batch_docs, embeddings)
+        ]
+
+        preprocessed.extend(batch_data)
+    
+        with open('preprocessed_documents.json', 'w') as out_f:
+            json.dump(preprocessed, out_f)
+
+        print('Processed ', i + len(batch_docs), ' of ', len(docs), ' documents')
+
 
 if __name__ == "__main__":
     main()
