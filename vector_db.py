786430067fdcb3df637bdb92b5e40e13deda00f3
boaz-ng <bkn2545@gmail.com>
2025-11-14 17:17:30 -0500

    Implement download of vector database

--- vector_db.py
+++ vector_db.py
@@ -1,28 +1,49 @@
 import faiss
 import json
+import os
+import numpy as np
 
 class VectorDatabase:
-    def __init__(self, embeddings_path='preprocessed_documents.json'):
-        with open(embeddings_path, 'r') as f:
+    def __init__(self, embeddings_path='preprocessed_documents.json', index_path='database.index'):
+
+        self.embeddings_path = embeddings_path
+        self.index_path = index_path
+
+        with open(self.embeddings_path, "r") as f:
             data = json.load(f)
 
-        self.queries = [doc['embedding'] for doc in data] # do i need to check types here? use numpy?
-        self.d = len(self.queries[0])
-        self.index = faiss.IndexFlatL2(d)
-        index.add(queries)
+        self.embeddings = np.array(
+            [doc["embedding"] for doc in data], dtype="float32"
+        )
+
+        self.dim = self.embeddings.shape[1]
+        self.index = self.load_index() 
+
+    def load_index(self):
+        if os.path.exists(self.index_path):
+            return faiss.read_index(self.index_path)
+
+        index = faiss.IndexFlatL2(self.dim)
+        index.add(self.embeddings)
+        faiss.write_index(index, self.index_path)
+        return index
 
     # search the database for the k nearest neighbors to a particular id
     def search_by_id(self, id, k=3):
         """Search using existing document (given id) as the query"""
-        query_embedding = queries[id].reshape(1, 768) # ensure shape (is this necessary?)
-        return index.search(query_embedding, k)
-        # return I # for now, return indices of nearest neighbors
+        query_embedding = self.embeddings[id].reshape(1, self.dim)
+        D, I = self.index.search(query_embedding, k)
+        return D, I
 
     def search_by_vector(self, embedding, k=3):
         """embedding: array of shape (1, 768)"""
-        return index.search(embedding, k)
+        embedding = np.array(embedding, dtype="float32").reshape(1, self.dim)
+        D, I = self.index.search(embedding, k)
+        return D, I
 
 if __name__ == "__main__":
+    # for now, we hard code this
     db = VectorDatabase()
     D, I = db.search_by_id(42, k=3)
     print("Nearest neighbor indices:", I)
+    print("Distances:", D)
